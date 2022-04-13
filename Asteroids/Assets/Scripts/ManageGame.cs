cc5815d3fb82e47421de740ff81ddf9c975db367
AhmadAziz0090 <72610079+AhmadAziz0090@users.noreply.github.com>
2022-04-13 22:17:00 +0100

    Added A Level System

--- Asteroids/Assets/Scripts/ManageGame.cs
+++ Asteroids/Assets/Scripts/ManageGame.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class ManageGame : MonoBehaviour
 {
     [SerializeField] private int numAsteroids; // total number of asteroids in the scene.
-
+    [SerializeField] private int levelNumber;
+    [SerializeField] GameObject asteroid;
 
 
     public void UpdateNumAsteroids(int change)
     {
         numAsteroids += change;
+
+        //Check for no asteroids.
+        if(numAsteroids <= 0)
+        {
+            //Start new Level.
+            Invoke("StartNewLevel", 3f);
+        }
+    }
+    
+    void StartNewLevel()
+    {
+        levelNumber++;
+        numAsteroids = levelNumber * 2;
+
+        //Spawn New Asteroids.
+        for (int i = 0; i < numAsteroids; i++)
+        {
+            Vector2 spawnPos = new Vector2(Random.RandomRange(-38, 38), 22f);
+            Instantiate(asteroid, spawnPos, Quaternion.identity);
+        }
     }
 }
